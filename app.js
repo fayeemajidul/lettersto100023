aa10d2b16f13d6aa0ad457815afe6a55b09193dc
Fayeem <76803185+fayeemajidul@users.noreply.github.com>
2025-06-12 10:53:47 -0400

    Refactored code to improve performance... and to make the coffee break longer.

---
+++ app.js
@@ -0,0 +1,5 @@
+// Optimize the algorithm for better performance
+function calculateEfficiency() {
+  // Just kidding, it's still pretty slow
+  return Math.random() > 0.5 ? "Quick!" : "I'll be right back...";
+}
