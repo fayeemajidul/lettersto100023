51ab2aee7f99156ef922bde8620154217aa67dc5
Fayeem <76803185+fayeemajidul@users.noreply.github.com>
2025-09-26 09:00:30 -0400

    Fixed the bug that was just a feature in disguise! 🐞✨

---
+++ debugging-magic.js
@@ -0,0 +1,11 @@
+// Initializing the magic wand for bug fixes
+function fixBugs() {
+    let spellsCast = 0;
+    while (spellsCast < 3) {
+        console.log("Casting 'Abracadabra'... 👉✨");
+        spellsCast++;
+    }
+    return "Bugs have vanished! Or maybe they're just hiding...";
+}
+
+console.log(fixBugs());
