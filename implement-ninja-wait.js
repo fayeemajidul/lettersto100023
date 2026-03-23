df05798aff19c2d72a787ba4ca2a6aecdee9277a
Fayeem <76803185+fayeemajidul@users.noreply.github.com>
2026-03-23 09:00:37 -0400

    Added stealth mode: now existing code can disappear without a trace!

---
+++ implement-ninja-wait.js
@@ -0,0 +1,9 @@
+function ninjaWait(milliseconds) {
+    console.log("Initiating stealth mode... 🥷");
+    setTimeout(() => {
+        console.log("Mission accomplished. Code is now invisible!");
+    }, milliseconds);
+}
+
+// Use it wisely, or not at all!
+ninjaWait(3000);
