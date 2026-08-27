b251f16e164f5035fd558ec22a0d20f63f77ecb3
Fayeem <76803185+fayeemajidul@users.noreply.github.com>
2026-08-27 09:01:01 -0400

    Added a function that finally explains why 6 is afraid of 7!

---
+++ quirkyMath.js
@@ -0,0 +1,10 @@
+function whyIsSixAfraidOfSeven() {
+    let fearLevel = Math.random();
+    if (fearLevel > 0.5) {
+        return "Because 7 8 (ate) 9!";
+    } else {
+        return "It's just a silly pun!";
+    }
+}
+
+console.log(whyIsSixAfraidOfSeven());
